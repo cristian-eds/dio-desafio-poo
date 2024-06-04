6a4604532f271b131e0ce495caee78cb5fa609f1
Cristian Eduardo <cristian.santos0308@gmail.com>
2024-06-03 22:49:43 -0300

    Criacao classes Bootcamp e Dev

--- src/edu/cristian/eds/dominio/Curso.java
+++ src/edu/cristian/eds/dominio/Curso.java
@@ -1,23 +1,9 @@
 package edu.cristian.eds.dominio;
 
-public class Curso {
+public class Curso extends Conteudo{
 	
-	private String titulo;
-	private String descricao;
 	private int cargaHoraria;
 	
-	public String getTitulo() {
-		return titulo;
-	}
-	public void setTitulo(String titulo) {
-		this.titulo = titulo;
-	}
-	public String getDescricao() {
-		return descricao;
-	}
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
 	public int getCargaHoraria() {
 		return cargaHoraria;
 	}
@@ -27,10 +13,13 @@ public class Curso {
 	
 	@Override
 	public String toString() {
-		return "Curso [titulo=" + titulo + ", descricao=" + descricao + ", cargaHoraria=" + cargaHoraria + "]";
+		return "Curso [titulo=" + getTitulo() + ", descricao=" + getDescricao() + ", cargaHoraria=" + cargaHoraria + "]";
+	}
+	@Override
+	public double calcularXp() {
+		return XP_PADRAO * cargaHoraria;
 	}
 	
-	
-	
+
 
 }
