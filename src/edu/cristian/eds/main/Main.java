7ebc4fa66b57637b76398f704db0dd913ea2a105
Cristian Eduardo <cristian.santos0308@gmail.com>
2024-06-03 22:55:57 -0300

    Finalizando classes

--- src/edu/cristian/eds/main/Main.java
+++ src/edu/cristian/eds/main/Main.java
@@ -2,7 +2,9 @@ package edu.cristian.eds.main;
 
 import java.time.LocalDate;
 
+import edu.cristian.eds.dominio.Bootcamp;
 import edu.cristian.eds.dominio.Curso;
+import edu.cristian.eds.dominio.Dev;
 import edu.cristian.eds.dominio.Mentoria;
 
 public class Main {
@@ -19,6 +21,19 @@ public class Main {
 		mentoria.setTitulo("Mentoria Java");
 		mentoria.setDescricao("Descricao Mentoria Java");
 		mentoria.setData(LocalDate.now());
+		
+		Bootcamp bootcamp = new Bootcamp();
+		bootcamp.setNome("Bootcamp Java");
+		bootcamp.setDescricao("Bootcamp");
+		bootcamp.getConteudos().add(curso);
+		bootcamp.getConteudos().add(mentoria);
+		
+		Dev dev1 = new Dev();
+		dev1.setNome("Crsitian");
+		dev1.inscreverBootcamp(bootcamp);
+		
+		System.out.println(dev1.getConteudosInscritos());
+		
 	}
 
 }
